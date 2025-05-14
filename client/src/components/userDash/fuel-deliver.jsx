fa8f7668c8b535c67b59ba4b4bdac7853fecf4fb
Waleed987 <waleed.younas.987@gmail.com>
2025-05-14 20:36:43 +0500

    added checkout button,which clicked scrolls the screen down , removed pic from that , added a new pic in fuel delivery page.

--- client/src/components/userDash/fuel-deliver.jsx
+++ client/src/components/userDash/fuel-deliver.jsx
@@ -878,9 +878,10 @@ export default function FuelDeliverySystem() {
                   </div>
                   <div className="md:w-1/2 h-64 bg-gray-300">
                     <img
-                      src="/api/placeholder/640/320"
-                      alt="Fuel delivery truck"
+                      src="https://images.unsplash.com/photo-1687203238072-2992045bedff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      alt="Car being refueled at a fuel pump"
                       className="w-full h-full object-cover"
+                      style={{objectPosition: 'center'}}
                     />
                   </div>
                 </div>
